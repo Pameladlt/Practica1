c76550cb6a1787bf3b92b1935dae5c2c87287c73
pamela <pamela_delatorrej@outlook.com>
2019-02-22 19:15:15 -0600

    soy pamela p1

---
+++ p1/src/main/java/com/mayab/proyecto/desarrollo/App.java
@@ -0,0 +1,13 @@
+package com.mayab.proyecto.desarrollo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
